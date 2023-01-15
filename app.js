95e12f9ae4849a6efeffae6ce26b6fc8cdb3a882
Priskin Zsuzsanna <priskin.zsuzsanna@gmail.com>
2023-01-15 21:58:36 +0100

    FrontendChallangeFinished - ver 1

---
+++ app.js
@@ -0,0 +1,22 @@
+const id = document.querySelector(".advice-id");
+const adviceText = document.querySelector(".advice-text");
+const dice = document.querySelector(".circle");
+const url = "https://api.adviceslip.com/advice";
+
+window.addEventListener("load", fetchData)
+
+dice.addEventListener("click", fetchData)
+
+function fetchData () {
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+        const dataId = data.slip.id
+        const dataAdvice = data.slip.advice
+
+        id.innerHTML = dataId;
+        adviceText.innerHTML = `
+        "${dataAdvice}"
+        `
+    })
+}
